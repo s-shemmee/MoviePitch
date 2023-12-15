1eda53642c71cae206126767bb944bb887715cba
shemmee <chaimalamirin@gmail.com>
2023-12-15 20:55:15 +0000

    update

---
+++ src/components/OutputComponent.vue
@@ -0,0 +1,18 @@
+<!-- OutputComponent.vue -->
+<template>
+  <section class="output-container" id="output-container">
+    <div id="output-img-container" class="output-img-container">
+      <img :src="imageUrl" alt="Movie Poster">
+    </div>
+    <h1 id="output-title">{{ title }}</h1>
+    <h2 id="output-stars">{{ stars }}</h2>
+    <p id="output-text">{{ text }}</p>
+    <button class="view-pitch-btn" @click="viewPitch" aria-label="View Pitch">
+      View Pitch
+    </button>
+  </section>
+</template>
+
+<script>
+
+</script>
---
+++ src/components/SetupComponent.vue
@@ -0,0 +1,24 @@
+<!-- SetupComponent.vue -->
+<template>
+  <section id="setup-container">
+    <div class="setup-inner">
+      <img src="@/assets/movieboss.png" alt="MovieBoss">
+      <div class="speech-bubble-ai" id="speech-bubble-ai">
+        <p id="movie-boss-text">
+          Give me a one-sentence concept, and I'll give you an eye-catching title, a synopsis the studios will love, a movie poster...
+          AND choose the cast!
+        </p>
+      </div>
+    </div>
+    <div class="setup-inner setup-input-container" id="setup-input-container">
+      <textarea v-model="userInput" id="setup-textarea" placeholder="An evil genius wants to take over the world using AI."></textarea>
+      <button @click="submitSetup" class="send-btn" id="send-btn" aria-label="send">
+        <img src="@/assets/send-btn-icon.png" alt="send">
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
