5ab265cde6dae76ff3ecd535c3fb3b8f98be4fd4
shemmee <chaimalamirin@gmail.com>
2023-12-17 22:03:05 +0000

    Dynamically update content in setup container during API calls

--- src/App.vue
+++ src/App.vue
@@ -28,3 +28,52 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+
+header {
+  background-color: var(--dark);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  z-index: 1000;
+}
+
+header>img {
+  height: 26px;
+}
+
+header>a {
+  color: var(--light);
+  font-size: 24px;
+  text-decoration: none;
+  padding: 16px 0;
+}
+
+header span {
+  font-weight: 700;
+}
+
+main {
+  padding: 16px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: var(--dark);
+  color: var(--light);
+  padding: 8px;
+  font-size: 14px;
+  text-align: center;
+  z-index: 1000;
+}
+</style>
--- src/components/MoviePitch.vue
+++ src/components/MoviePitch.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section v-if="showSetup" id="setup-container">
+    <section id="setup-container">
       <div class="setup-inner">
         <img src="@/assets/movieboss.png" alt="MovieBoss">
         <div class="speech-bubble-ai" ref="speechBubble">
@@ -17,7 +17,7 @@
       </div>
     </section>
 
-    <section v-else class="output-container" id="output-container">
+    <section class="output-container" id="output-container">
       <div id="output-img-container" class="output-img-container">
         <img :src="imageUrl" alt="Movie Poster">
       </div>
@@ -31,44 +31,36 @@
   </div>
 </template>
 
-<script>
-import { ref } from 'vue';
-import { Configuration, OpenAIApi } from 'openai';
+<script setup>
+  import { ref } from 'vue';
+  import { Configuration, OpenAIApi } from 'openai';
 
-export default {
-  setup() {
-    const showSetup = ref(true);
-    const userInput = ref('');
-    const imageUrl = ref('');
-    const title = ref('');
-    const stars = ref('');
-    const text = ref('');
+  const userInput = ref("");
+  const imageUrl = ref("");
+  const title = ref("");
+  const stars = ref("");
+  const text = ref("");
 
-    const movieBossText = ref(null);
-    const setupInputContainer = ref(null);
+  const setupInputContainer = ref(null);
+  const movieBossText = ref(null);
 
-    const configuration = new Configuration({
-      apiKey: 'YOUR_OPENAI_API_KEY',
-    });
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
 
-    const openai = new OpenAIApi(configuration);
+  const openai = new OpenAIApi(configuration);
 
-    const submitSetup = async () => {
-      if (userInput.value) {
-        setupInputContainer.value.innerHTML = `<img src="@/assets/loading.svg" class="loading" id="loading">`;
-        movieBossText.value.innerText = `Ok, just wait a second while my digital brain digests that...`;
-        await fetchBotReply(userInput.value);
-        await fetchSynopsis(userInput.value);
-      }
-    };
+  const submitSetup = () => {
+    if (userInput.value) {
+      setupInputContainer.value.innerHTML = `<img src="@/assets/loading.svg" class="loading" id="loading">`;
+      movieBossText.value.innerText = `Ok, just wait a second while my digital brain digests that...`;
+      fetchBotReply(userInput.value);
+      fetchSynopsis(userInput.value);
+    }
+  };
 
-    const viewPitch = () => {
-      showSetup.value = false;
-      movieBossText.value.innerText = `This idea is so good I'm jealous! It's gonna make you rich for sure! Remember, I want 10% 💰`;
-    };
-
-    const fetchBotReply = async (outline) => {
-      try {
+  const fetchBotReply = async (outline) => {
+    try {
         const response = await openai.createCompletion({
           model: 'text-davinci-003',
           prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
@@ -93,34 +85,34 @@ export default {
         console.error('Error fetching bot reply:', error);
         // Handle errors as needed
       }
-    };
+  };
 
-    const fetchSynopsis = async (outline) => {
-      try {
-        const response = await openai.createCompletion({
-          model: 'text-davinci-003',
-          prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
-          ###
-          outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
-          synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.  
-          ###
-          outline: ${outline}
-          synopsis: 
-          `,
-          max_tokens: 700,
-        });
+  const fetchSynopsis = async (outline) => {
+    try {
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
+        ###
+        outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+        synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.  
+        ###
+        outline: ${outline}
+        synopsis: 
+        `,
+        max_tokens: 700,
+      });
 
-        const synopsis = response.data.choices[0].text.trim();
-        text.value = synopsis;
-        await fetchTitle(synopsis);
-        await fetchStars(synopsis);
-      } catch (error) {
-        console.error('Error fetching synopsis:', error);
-        // Handle errors as needed
-      }
-    };
+      const synopsis = response.data.choices[0].text.trim();
+      text.value = synopsis;
+      await fetchTitle(synopsis);
+      await fetchStars(synopsis);
+    } catch (error) {
+      console.error('Error fetching synopsis:', error);
+      // Handle errors as needed
+    } 
+  };
 
-    const fetchTitle = async (synopsis) => {
+  const fetchTitle = async (synopsis) => {
       try {
         const response = await openai.createCompletion({
           model: 'text-davinci-003',
@@ -209,18 +201,191 @@ export default {
       }
     };
 
-    return {
-      showSetup,
-      userInput,
-      imageUrl,
-      title,
-      stars,
-      text,
-      movieBossText,
-      setupInputContainer,
-      submitSetup,
-      viewPitch,
+    const viewPitch = () => {
+      document.getElementById('setup-container').style.display = 'none';
+      document.getElementById('output-container').style.display = 'flex';
+      movieBossText.value.innerText = `This idea is so good I'm jealous! It's gonna make you rich for sure! Remember, I want 10% 💰`;
     };
-  },
-};
 </script>
+
+
+<style scoped>
+
+section {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--light);
+  border-radius: var(--border-rad-lg);
+  padding: 16px;
+  box-shadow: 0px 1px 18px 3px var(--dark);
+  max-width: 500px;
+  width: calc(100% - 32px);
+  z-index: 1000;
+}
+
+/* Setup */
+
+.setup-inner {
+  display: flex;
+  justify-content: space-around;
+  padding: 24px 8px;
+  min-height: 234px;
+}
+
+.setup-inner>img {
+  width: 40%;
+  filter: drop-shadow(3px 2px 3px var(--medium-dark));
+  align-self: center;
+}
+
+.setup-input-container {
+  min-height: 74px;
+}
+
+.speech-bubble-ai {
+  max-width: 55%;
+  min-height: 124px;
+  border-radius: var(--border-rad-lg);
+  position: relative;
+  margin: 0;
+  border: 3px solid var(--medium-dark);
+  background-color: var(--white);
+  align-self: flex-start;
+  display: flex;
+  align-items: center;
+}
+
+.speech-bubble-ai:before {
+  content: "";
+  position: absolute;
+  border-left: 9px solid transparent;
+  border-right: 9px solid var(--medium-dark);
+  border-top: 9px solid var(--medium-dark);
+  border-bottom: 9px solid transparent;
+  left: -21px;
+  top: 64px;
+}
+
+.speech-bubble-ai:after {
+  content: "";
+  position: absolute;
+  border-left: 7px solid transparent;
+  border-right: 7px solid var(--white);
+  border-top: 7px solid var(--white);
+  border-bottom: 7px solid transparent;
+  left: -11px;
+  top: 68px;
+}
+
+.speech-bubble-ai>p {
+  padding: 0 1.3em;
+  color: var(--dark);
+  font-size: 14px;
+}
+
+textarea {
+  background-color: var(--light-grey);
+  padding: 8px;
+  border: none;
+  border-top-right-radius: 0;
+  border-top-left-radius: var(--border-rad-lg);
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: var(--border-rad-lg);
+  width: 100%;
+  resize: none;
+  min-height: 40px;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+textarea::placeholder {
+  color: var(--medium-dark);
+  font-size: 14px;
+  opacity: 0.8;
+}
+
+/* Larger mobiles+ */
+
+@media(min-width: 380px) {
+  .setup-input-container {
+    padding-top: 0;
+  }
+
+  .speech-bubble-ai:before {
+    top: 92px;
+  }
+
+  .speech-bubble-ai:after {
+    top: 96px;
+  }
+
+  .speech-bubble-ai>p {
+    font-size: 16px;
+  }
+
+  textarea::placeholder {
+    font-size: 16px;
+    opacity: 0.8;
+  }
+}
+
+/* Buttons & SVG */
+
+button {
+  border: none;
+  background: var(--pink);
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: var(--dark);
+}
+
+.send-btn {
+  border-top-right-radius: var(--border-rad-lg);
+  border-bottom-right-radius: var(--border-rad-lg);
+  min-width: 50px;
+}
+
+.send-btn>img {
+  width: 1.6em;
+  vertical-align: middle;
+}
+
+.view-pitch-btn {
+  color: var(--light);
+  border-radius: var(--border-rad-lg);
+  padding: 16px;
+  margin: 4px auto;
+  display: block;
+  font-size: 18px;
+}
+
+.view-pitch-btn:hover {
+  box-shadow: 1px 1px 5px 1px var(--medium-dark);
+}
+
+img.loading {
+  max-width: 40px;
+  filter: none;
+}
+
+/* Output */
+
+.output-container {
+  display: none;
+  flex-direction: column;
+  margin: 16px auto;
+  color: var(--dark);
+  padding: 16px;
+}
+
+.output-img-container>img {
+  width: 100%;
+  border-radius: var(--border-rad-lg);
+  box-shadow: 1px 1px 5px 1px var(--dark);
+}
+
+</style>
